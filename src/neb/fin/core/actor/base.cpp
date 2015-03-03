0018f8797f0827cb1543e1ebfef86043aed709c1
chuck <charlesrymal@gmail.com>
2015-03-02 19:55:52 -0800

    same

--- src/neb/fin/core/actor/base.cpp
+++ src/neb/fin/core/actor/base.cpp
@@ -1,18 +1,3 @@
-#include <gal/stl/deleter.hpp>
-#include <gal/stl/verbosity.hpp>
-
-#include <neb/fnd/app/Base.hpp>
-#include <neb/fnd/plug/gfx/core/shape/Base.hpp>
-#include <neb/fnd/plug/gfx/core/actor/Base.hpp>
-#include <neb/fnd/plug/gfx/core/actor/util/decl.hpp>
-
-#include <neb/fnd/plug/phx/core/actor/Base.hpp>
-
-#include <neb/fin/core/actor/rigidactor/base.hpp>
-#include <neb/fin/core/shape/box.hpp>
-#include <neb/fin/core/shape/HeightField.hpp>
-
-#include <neb/fin/core/actor/base.hpp>
 
 typedef neb::fin::core::actor::base THIS;
 
